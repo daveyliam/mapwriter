11f52007092a89011b7540511b47eabdb1762b85
jk-5 <jeffreykog@me.com>
2014-02-23 19:03:16 +0100

    Gradelization

--- forge/CommonProxy.java
+++
@@ -1,5 +0,0 @@
-package mapwriter.forge;
-
-public class CommonProxy {
-	public void init(MwConfig config) {}
-}
--- region/IChunk.java
+++
@@ -1,8 +0,0 @@
-package mapwriter.region;
-
-public interface IChunk {
-	public int getBlockAndMetadata(int x, int y, int z);
-	public int getBiome(int x, int z);
-	public int getLightValue(int x, int y, int z);
-	public int getMaxY();
-}
