04aab82ed3f4e2f30cc502d94682c3ba8bd24f89
Vectron <thijs_bloebaum@hotmail.com>
2015-08-16 18:09:59 +0200

    Fixes nullpointer crash at startup with some mods. Upped version number Fixes #5 #6

--- src/main/java/mapwriter/forge/MwForge.java
+++ src/main/java/mapwriter/forge/MwForge.java
@@ -21,7 +21,7 @@ import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.TickEvent;
 import cpw.mods.fml.common.network.FMLNetworkEvent;
 
-@Mod(modid="MapWriter", name="MapWriter", version="2.1.7", acceptableRemoteVersions = "*")
+@Mod(modid="MapWriter", name="MapWriter", version="2.1.8", acceptableRemoteVersions = "*")
 public class MwForge {
 	
 	@Instance("MapWriter")
