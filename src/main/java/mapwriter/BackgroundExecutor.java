ddd387a9ea5326db39a85ed57c44e8d0a8b28af4
Vectron <thijs_bloebaum@hotmail.com>
2015-10-25 01:25:15 +0200

    comment fix

--- src/main/java/mapwriter/BackgroundExecutor.java
+++ src/main/java/mapwriter/BackgroundExecutor.java
@@ -13,34 +13,40 @@ import mapwriter.tasks.UpdateSurfaceChunksTask;
 import mapwriter.util.Logging;
 import net.minecraft.world.ChunkCoordIntPair;
 
+// @formatter:off
 /*
- * This class handles executing and managing 'tasks'. A single background thread
- * runs tasks in the sequence they are added (via addTask()). A linked list FIFO
- * queue of every unfinished task is processed every time the processTaskQueue
- * method is called. processTaskQueue checks the task at the front of the queue
- * to see if the background thread has processed it. If it the task is complete
- * processTaskQueue calls the onComplete() method of the task and removes it
- * from the queue. If it is not complete the task is added to the front of the
- * queue again. In this way the tasks are always processed sequentially, in the
- * order they were added to the queue.
- *
- * Tasks are extensions of the base Task class. There are two abstract methods
- * which must be overwritten by the extending class. void run() Is executed in
- * the background thread when the executor reaches this task. void onComplete()
- * Is called by processTaskQueue() when the task is done (after the run method
- * is complete). This method runs in the main thread so is a good place to copy
- * the results of the run() method.
- *
- * The run() method of a task added to the queue is guaranteed to be run before
- * the run() method of the next task added. Likewise the onComplete() method of
- * the first task is guaranteed to be run before the onComplete() of the second
- * task. However the run() method of any class added after a Task may be
- * executed before the onComplete() method of the earlier Task is called.
- *
- * e.g. addTask(Task1) addTask(Task2) addTask(Task3)
- *
- * may run in the order: Task1.run() Task2.run() Task1.onComplete() Task3.run()
- * Task2.onComplete() Task3.onComplete()
+ * This class handles executing and managing 'tasks'.
+ * A single background thread runs tasks in the sequence they are added (via addTask()).
+ * A linked list FIFO queue of every unfinished task is processed every time the processTaskQueue method is called.
+ * processTaskQueue checks the task at the front of the queue to see if the background thread has processed it.
+ * If it the task is complete processTaskQueue calls the onComplete() method of the task and removes it from the queue.
+ * If it is not complete the task is added to the front of the queue again.
+ * In this way the tasks are always processed sequentially, in the order they were added to the queue.
+ * 
+ * Tasks are extensions of the base Task class.
+ * There are two abstract methods which must be overwritten by the extending class.
+ *   void run()
+ *     Is executed in the background thread when the executor reaches this task.
+ *   void onComplete()
+ *     Is called by processTaskQueue() when the task is done (after the run method is complete).
+ * 	This method runs in the main thread so is a good place to copy the results of the run() method.
+ * 
+ * The run() method of a task added to the queue is guaranteed to be run before the run() method of the next task
+ * added. Likewise the onComplete() method of the first task is guaranteed to be run before the onComplete() of the second
+ * task. However the run() method of any class added after a Task may be executed before the onComplete() method of
+ * the earlier Task is called.
+ * 
+ * e.g. addTask(Task1)
+ *      addTask(Task2)
+ * 	 addTask(Task3)
+ * 	 
+ * may run in the order:
+ *     Task1.run()
+ * 	Task2.run()
+ * 	  Task1.onComplete()
+ * 	Task3.run()
+ * 	  Task2.onComplete()
+ * 	  Task3.onComplete()
  */
 
 public class BackgroundExecutor
